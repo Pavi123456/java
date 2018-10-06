8ef1489130129979544ccda9d811917120664c7b
Pavi123456 <43901614+Pavi123456@users.noreply.github.com>
2018-10-06 14:57:26 +0530

    Update EvenOdd.java

--- EvenOdd.java
+++ EvenOdd.java
@@ -11,6 +11,8 @@ public class EvenOdd {
 
         if(num % 2 == 0)
             System.out.println( " even");
+        else if(num<0)
+            System.out.println("Invalid")
         else
             System.out.println( "  odd");
     }
