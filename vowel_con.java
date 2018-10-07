324d67f254880eb7edf6721245b62b7daa72c495
Pavi123456 <43901614+Pavi123456@users.noreply.github.com>
2018-10-07 19:48:12 +0530

    Add files via upload

---
+++ vowel_con.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class vowel_con
+{
+	public static void main(String[ ] arg)
+	{
+	int i=0;
+	Scanner sc=new Scanner(System.in);
+	char ch=sc.next( ).charAt(0);	 
+	//char ch=sc.nextChar();
+	switch(ch)
+	{
+	 case  'a' :
+	 case 'e'  :
+	 case 'i'   :
+	 case 'o'  :
+	 case 'u'  :
+	 case  'A' :
+	 case 'E'  :
+	 case 'I'   :
+	 case 'O'  :
+	 case 'U'  :i++;
+	}
+	if(i==1)
+	System.out.println("Vowel");
+	else 
+		if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+		System.out.println("Consonent");
+		else
+		System.out.println("Invalid");		
+	}
+}
