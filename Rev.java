16676cf056e44d0d35e949d302983542c1e68731
Pavi123456 <43901614+Pavi123456@users.noreply.github.com>
2018-10-27 21:29:15 +0530

    Add files via upload

---
+++ Rev.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Rev
+{
+   public static void main(String args[])
+   {
+      String orig, rev="";
+      int i, len;
+      Scanner scan = new Scanner(System.in);
+      orig = scan.nextLine();
+      
+      len = orig.length();
+	  
+      for(i=len-1; i>=0; i--)
+      {
+          rev = rev + orig.charAt(i);
+      }
+	  
+        System.out.print(rev);
+   }
+}
