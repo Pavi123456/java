be7fef485fd0100c6fd0c8fa4e6601f194f23f81
Pavi123456 <43901614+Pavi123456@users.noreply.github.com>
2018-10-27 22:36:13 +0530

    Add files via upload

---
+++ STR.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class STR
+{
+    public static void main(String args[])
+    {
+        String str;
+        Scanner scan = new Scanner(System.in);
+        str= scan.nextLine();
+		        System.out.print(str);
+    }
+}
