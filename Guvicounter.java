f7f68e78149d9aff990af1df7e66a0ecfc3afcd8
Pavi123456 <43901614+Pavi123456@users.noreply.github.com>
2019-01-09 22:25:18 +0530

    Add files via upload

---
+++ Guvicounter.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Guvicounter {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+	//	System.out.println("enter the n nos");
+		int n=in.nextInt();
+      System.out.println(n);
+	}
+
+}
