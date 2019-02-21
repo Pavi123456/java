b9a78d240075f5b0a471a52b6ae3c3047919421a
Pavi123456 <43901614+Pavi123456@users.noreply.github.com>
2019-02-21 11:49:52 +0530

    Add files via upload

---
+++ min_max_array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Arrays</title>
+</head>
+<body>
+	<h1>Array Functions</h1>
+	<pre>PHP ARRAY
+		<?php
+$myArr=array(100,2,30,45,58,69,17);
+		?>
+		<p>Min:<?php echo min($myArr); ?></p>
+		<p>Max:<?php echo max($myArr); ?></p>
+		
+	</pre>
+
+</body>
+</html>>
